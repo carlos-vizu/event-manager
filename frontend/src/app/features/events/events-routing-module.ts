f33aa59887d1ee6be87ca658d33d3a5e19985730
carlos_vizu <kaduvizu@hotmail.com>
2025-11-02 18:56:34 -0300

    feat(routes): creating routing for components: List, Create, Edit and Detail

--- frontend/src/app/features/events/events-routing-module.ts
+++ frontend/src/app/features/events/events-routing-module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class EventsRoutingModule { }
+const routes: Routes = [
+  { path: 'events', component: ListComponent },
+  { path: 'events/new', component: CreateComponent },
+  { path: 'events/:id/edit', component: EditComponent },
+  { path: 'events/:id', component: DetailComponent },
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: '**', redirectTo: '/events' }
+];
