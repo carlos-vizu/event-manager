2fc60c752c93170b5a27763b9535efff30dc76fd
carlos_vizu <kaduvizu@hotmail.com>
2025-11-02 18:27:28 -0300

    feat(service): implemeting event.service.ts with API calls and with the API URL

--- frontend/src/app/core/services/event.service.ts
+++ frontend/src/app/core/services/event.service.ts
@@ -1,3 +1,39 @@
-export interface Event {
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Event } from '../models/event.model';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  private apiUrl = 'http://localhost:8080/api/events';
+
+  private events: Event[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  listEvents(page: number = 0, size: number = 10): Observable<any> {
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+
+  getEvent(id: number): Observable<Event> {
+    return this.http.get<Event>(`${this.apiUrl}/${id}`);
+  }
+
+  createEvent(event: Event): Observable<Event> {
+    return this.http.post<Event>(this.apiUrl, event);
+  }
+
+  updateEvent(id: number, event: Event): Observable<Event> {
+    return this.http.put<Event>(`${this.apiUrl}/${id}`, event);
+  }
+
+  deleteEvent(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
