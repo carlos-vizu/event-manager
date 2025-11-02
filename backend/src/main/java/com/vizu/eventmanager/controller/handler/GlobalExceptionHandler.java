bdb818c2381a12cfbbe2f07599ac7d441c83b863
carlos_vizu <kaduvizu@hotmail.com>
2025-11-02 17:52:26 -0300

    feat(controller): create GlabalExceptionHandler

---
+++ backend/src/main/java/com/vizu/eventmanager/controller/handler/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.vizu.eventmanager.controller.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
