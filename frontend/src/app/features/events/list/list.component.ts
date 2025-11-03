548d1c6ecc878279e84a60339a4bb108454a18e0
carlos_vizu <kaduvizu@hotmail.com>
2025-11-03 00:23:44 -0300

    feat(component): implementing on component 'list' the card component listing, style and html

--- frontend/src/app/features/events/list/list.component.ts
+++ frontend/src/app/features/events/list/list.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { EventService } from '../../../core/services/event.service';
+import { Event } from '../../../core/models/event.model';
 
 @Component({
-  selector: 'app-list',
+  selector: 'app-event-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink, MatListModule, MatPaginatorModule],
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
-  constructor() { }
+export class ListComponent {
+  events: Event[] = [];
+  totalElements = 0;
+  pageSize = 10;
+  pageIndex = 0;
 
-  ngOnInit(): void { }
+  constructor(private eventService: EventService) {
+    this.loadEvents();
+  }
+
+  loadEvents(page: number = 0, size: number = this.pageSize) {
+    this.eventService.listEvents(page, size).subscribe((res) => {
+      this.events = res.content || res;
+      this.totalElements = res.totalElements || this.events.length;
+    });
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.loadEvents(this.pageIndex, this.pageSize);
+  }
+
+  deleteEvent(id: number) {
+    if (confirm('Are you sure you want to delete this event?')) {
+      this.eventService.deleteEvent(id).subscribe(() => {
+        this.loadEvents(this.pageIndex, this.pageSize);
+      });
+    }
+  }
 }
