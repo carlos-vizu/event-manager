7d2dd8bfd5a15ce0655e971f8c643f13a5927d8b
carlos_vizu <kaduvizu@hotmail.com>
2025-11-03 02:28:21 -0300

    feat(swagger): change EventController with the operation descriptors for swagger endpoint

--- backend/src/main/java/com/vizu/eventmanager/application/controller/EventController.java
+++ backend/src/main/java/com/vizu/eventmanager/application/controller/EventController.java
@@ -3,6 +3,11 @@ package com.vizu.eventmanager.application.controller;
 import com.vizu.eventmanager.application.controller.dto.request.EventRequestDTO;
 import com.vizu.eventmanager.application.controller.dto.response.EventResponseDTO;
 import com.vizu.eventmanager.domain.service.EventService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,26 +25,43 @@ public class EventController {
         this.service = service;
     }
 
+    @Operation(summary = "Retorna uma lista de eventos ativos", description = "Lista paginada de eventos")
     @GetMapping
     public ResponseEntity<Page<EventResponseDTO>> listAllActives(Pageable pageable) {
         return ResponseEntity.ok(service.listAllActives(pageable));
     }
 
+    @Operation(summary = "Busca evento por ID", description = "Retorna os detalhes de um evento específico pelo seu ID.")
     @GetMapping("/{id}")
     public ResponseEntity<EventResponseDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
+    @Operation(summary = "Cria um novo evento", description = "Cria um evento com os dados fornecidos no corpo da requisição.",
+            responses = {
+                    @ApiResponse(description = "Evento criado com sucesso", responseCode = "200", content = @Content(schema = @Schema(implementation = EventResponseDTO.class))),
+                    @ApiResponse(description = "Erro de validação", responseCode = "400", content = @Content),
+            })
     @PostMapping
     public ResponseEntity<EventResponseDTO> create(@Valid @RequestBody EventRequestDTO dto) {
         return ResponseEntity.ok(service.create(dto));
     }
 
+    @Operation(summary = "Atualiza um evento existente", description = "Atualiza os dados de um evento existente pelo ID informado.",
+            responses = {
+                @ApiResponse(description = "Evento deletado com sucesso", responseCode = "204", content = @Content),
+                @ApiResponse(description = "Evento não encontrado", responseCode = "404", content = @Content),
+            }
+    )
     @PutMapping("/{id}")
     public ResponseEntity<EventResponseDTO> update(@PathVariable Long id, @Valid @RequestBody EventRequestDTO dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
 
+    @Operation(summary = "Deleta um evento", description = "Remove um evento pelo ID informado.",
+            responses = {
+                    @ApiResponse(description = "Evento deletado com sucesso", responseCode = "204", content = @Content)
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
