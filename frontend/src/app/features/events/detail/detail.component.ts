6a8b4bbb8ec9dcbce535c55a66dc8c80aa32d564
carlos_vizu <kaduvizu@hotmail.com>
2025-11-03 00:51:58 -0300

    feat(component): implementing on component 'detail' the card component listing, style and html

--- frontend/src/app/features/events/detail/detail.component.ts
+++ frontend/src/app/features/events/detail/detail.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { EventService } from '../../../core/services/event.service';
+import { Event } from '../../../core/models/event.model';
 
 @Component({
-  selector: 'app-detail',
-  imports: [],
+  selector: 'app-event-detail',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss',
+  styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  event?: Event;
 
+  constructor(private route: ActivatedRoute, private eventService: EventService) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.eventService.getEvent(id).subscribe((res) => (this.event = res));
+  }
 }
